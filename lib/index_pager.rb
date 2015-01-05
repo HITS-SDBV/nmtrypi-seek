80d8f0690dd8749773f807602000872d36d64251
Lihua An <Lihua.An@h-its.org>
2015-01-05 11:50:58 +0100

    fix merging errors

--- lib/index_pager.rb
+++ lib/index_pager.rb
@@ -22,10 +22,11 @@ module IndexPager
 
   end
 
-  def find_assets  action="view"
+  def index_with_facets?
     Seek::Config.faceted_browsing_enabled && Seek::Config.facet_enable_for_pages[self.controller_name.downcase] && ie_support_faceted_browsing?
   end
 
+  def find_assets
     begin
       fetch_and_filter_assets
     rescue ActiveRecord::RecordNotFound
@@ -45,7 +46,7 @@ module IndexPager
   def fetch_all_viewable_assets
     model_class=self.controller_name.classify.constantize
     if model_class.respond_to? :all_authorized_for
-      found = model_class.all_authorized_for action, User.current_user
+      found = model_class.all_authorized_for "view", User.current_user
     else
       found = model_class.respond_to?(:default_order) ? model_class.default_order : model_class.all
     end
