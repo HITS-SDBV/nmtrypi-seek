5dab1e7a0f6b20b4a791d4479da29b767d4a55eb
Lihua An <Lihua.An@h-its.org>
2015-01-23 11:58:52 +0100

    search_matches_lines: refactor code

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -54,4 +54,51 @@ module SearchHelper
     [internal_resource_hash, external_resource_hash]
   end
 
+  def search_extractable_items items, search_query
+
+    sheet_array = []
+    items = items.select { |item| item.is_asset? && item.content_blob.is_extractable_spreadsheet? }
+    results = items.select do |object|
+      workbook = Rails.cache.fetch(object.content_blob.cache_key) do
+        object.spreadsheet
+      end
+      xml = object.spreadsheet_xml
+      doc = LibXML::XML::Parser.string(xml).parse
+      doc.root.namespaces.default_prefix="ss"
+
+      #@origin_query = "Final Concentration,  LmPTR1 <=  30"
+      # @origin_query  ="Final Concentration, LmPTR1 [µM]"
+      search_query = search_query ? search_query.gsub(/\s+/, " ").strip : ""
+      operators = ["<", "<=", ">", ">=", "="]
+      search_operator = operators.select { |o| search_query.index(o) }.max_by(&:length)
+      if search_operator
+        search_arr = search_query.split(search_operator)
+        search_field_name = search_arr.first
+        search_value = search_arr.last
+        search_operator = "=" if search_field_name == search_value
+
+        head_cells = doc.find("//ss:sheet[@hidden='false' and @very_hidden='false']/ss:rows/ss:row/ss:cell").find_all { |cell| cell.content.gsub(/\s+/, " ").strip.match(/#{search_field_name}/i) }
+        unless head_cells.blank?
+          head_cell = head_cells[0]
+          #head_sheet = head_cell.parent.parent.parent
+          head_col = head_cell.attributes["column"]
+          cells = doc.find("//ss:sheet[@hidden='false' and @very_hidden='false']/ss:rows/ss:row/ss:cell[@column=#{head_col}][text() #{search_operator} #{search_value}]").find_all
+
+        end
+      else
+        cells = doc.find("//ss:sheet[@hidden='false' and @very_hidden='false']/ss:rows/ss:row/ss:cell").find_all { |cell| cell.content.downcase.include? search_query.downcase }
+      end
+      unless cells.blank?
+        cell_groups = cells.group_by { |c| c.parent.try(:parent).try(:parent).try(:attributes).to_h["name"] }
+        sheet_array |= cell_groups.map do |sheet_name, match_cells|
+          sheet = workbook.sheets.detect { |sh| sheet_name.downcase == sh.name.downcase }
+          rows_nums = match_cells.map { |c| c.attributes["row"].to_i }
+          col_nums = match_cells.map { |c| c.attributes["column"].to_i }
+          [sheet, rows_nums, col_nums, object.id]
+        end
+      end
+      !sheet_array.empty?
+    end
+    return results, sheet_array
+  end
 end
