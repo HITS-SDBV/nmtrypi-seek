40737162e4aad6f11c39160500f888216d1c040b
Lihua An <Lihua.An@h-its.org>
2015-03-20 15:47:50 +0100

    search_matched: fix wrong cached id_smiles hash

--- lib/seek/data/compounds_extraction.rb
+++ lib/seek/data/compounds_extraction.rb
@@ -14,36 +14,37 @@ module Seek
       private
 
       def get_compound_id_smiles_hash
-        id_smile_hash = {}
+        id_smiles_hash = {}
         DataFile.all.each do |df|
-          Rails.cache.fetch("#{df.content_blob.cache_key}-compound-id-smile-hash") do
-            #temporiably only excels
-            if df.content_blob.is_extractable_spreadsheet?
-              xml = df.spreadsheet_xml
-              doc = LibXML::XML::Parser.string(xml).parse
-              doc.root.namespaces.default_prefix="ss"
-              compound_id_cells = get_column_cells doc, "compound"
-              smiles_cells = get_column_cells doc, "smile"
-              compound_id_cells.each do |id_cell|
-                row_index = id_cell.attributes["row"]
-                smile = smiles_cells.detect { |cell| cell.attributes["row"] == row_index }.try(:content)
-                if id_cell && is_standard_compound_id?(id_cell.content) && !smile.blank?
-                  standardized_content = Seek::Search::SearchTermStandardize.to_standardize(id_cell.content)
-                  id_smile_hash[standardized_content] = smile
-                end
-              end
-              id_smile_hash.empty?? nil : id_smile_hash
-            else
-              nil
-            end
-          end
+          id_smiles_hash.merge! get_compound_id_smiles_hash_per_file(df)
         end
         #sort by key
-        id_smile_hash = id_smile_hash.sort_by{|k,v| k.to_s}.to_h
-        #id_smile_hash.empty?? nil : id_smile_hash
+        id_smiles_hash.sort_by{|k,v| k.to_s}.to_h
       end
 
+      def get_compound_id_smiles_hash_per_file data_file
+        Rails.cache.fetch("#{data_file.content_blob.cache_key}-compound-id-smile-hash") do
+          id_smiles_hash = {}
+          #temporiably only excels
+          if data_file.content_blob.is_extractable_spreadsheet?
+            xml = data_file.spreadsheet_xml
+            doc = LibXML::XML::Parser.string(xml).parse
+            doc.root.namespaces.default_prefix="ss"
+            compound_id_cells = get_column_cells doc, "compound"
+            smiles_cells = get_column_cells doc, "smile"
+            compound_id_cells.each do |id_cell|
+              row_index = id_cell.attributes["row"]
+              smile = smiles_cells.detect { |cell| cell.attributes["row"] == row_index }.try(:content)
+              if id_cell && is_standard_compound_id?(id_cell.content) && !smile.blank?
+                standardized_content = Seek::Search::SearchTermStandardize.to_standardize(id_cell.content)
+                id_smiles_hash[standardized_content] = smile
+              end
+            end
+          end
+          id_smiles_hash
+        end
 
+      end
       def get_column_cells doc, column_name
         head_cells = doc.find("//ss:sheet[@hidden='false' and @very_hidden='false']/ss:rows/ss:row/ss:cell").find_all { |cell| cell.content.gsub(/\s+/, " ").strip.match(/#{column_name}/i) }
         body_cells = []
