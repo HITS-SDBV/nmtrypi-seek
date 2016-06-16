a3fc154b14348d9d2c5edc0288ce034bfc1714c5
woetzens <nils.woetzel@h-its.org>
2016-06-16 20:03:52 +0200

    replace broken migrations with a new VagrantSetup migration updated Schema version to this new migration use db:migrate in ansible_site.yml setup [ci skip]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141204122730) do
+ActiveRecord::Schema.define(:version => 20160616172811) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
